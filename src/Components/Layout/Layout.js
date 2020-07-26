bb277c699fbaba87df5db549ab56dfb2b0844f35
Rawan Akram <rawan.akram27@yaoo.com>
2020-07-26 17:43:28 +0300

    create Toolbar

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from  '../../hoc/Auxi'
 import classes from '../Layout/Layout.css'
+import Toolbar from '../../Components/Navigation/Toolbar/Toolbar'
 const Layout = (props)=>(
    <Aux>
    <div>
-     Toolbar and Drow and sdjwsd
+     <Toolbar/>
    </div>
    <main className={classes.Content}>{props.children}</main>
 </Aux>
